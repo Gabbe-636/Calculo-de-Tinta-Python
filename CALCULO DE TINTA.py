1cf4592964e2415a31de04b694207e9c305fc9e4
Gabriel F. K <112198308+Gabbe-636@users.noreply.github.com>
2022-09-12 00:41:38 -0300

    Add files via upload

---
+++ CALCULO DE TINTA.py
@@ -0,0 +1,7 @@
+lar = int(input('Qual a largura da parede?'))
+alt = int(input('Qual a altura da parede?'))
+area = lar*alt
+print('A área é de {}'.format(area))
+
+lit = area/2
+print('Serão necessários {} litros de tinta.'.format(lit))
